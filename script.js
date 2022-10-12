03c6ecd8b3657117b602556404e17f4db81e8ea3
Max Berest <mberest@gmail.com>
2022-10-12 15:43:32 -0400

    Update script.js

--- script.js
+++ script.js
@@ -189,7 +189,7 @@ document.addEventListener("DOMContentLoaded", ()=> {
         catch (e) { }
     });
 
-    fetch('https://7te1h9vgtd.execute-api.us-east-1.amazonaws.com/prod?RideId=2', {headers: {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'} })
+    fetch('https://7te1h9vgtd.execute-api.us-east-1.amazonaws.com/prod?RideId=2', {headers: {'Content-Type': 'application/json'} })
         .then((response) => response.json())
         .then((data) => console.log(data))
         .then(()=> toggleButton.innerHTML = "hello");
